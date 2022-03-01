f1204deb5cc8b808f3f4f563dac3c7c770dad82b
Carnagion <carnagion.games@gmail.com>
2022-03-01 10:31:57 +0000

    Add partial class definition

---
+++ IEnumerableExtensions.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    /// <summary>
+    /// Provides a set of static (Shared in Visual Basic) methods for querying objects that implement <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    public static partial class IEnumerableExtensions
+    {
+    }
+}
