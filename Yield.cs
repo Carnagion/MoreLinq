534e2bf6bfaa99af481494bb8647a83bc377e6d1
Carnagion <carnagion.games@gmail.com>
2022-03-01 10:31:36 +0000

    Add Yield<T>

---
+++ Yield.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+
+namespace System.Linq
+{
+    public static partial class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Creates a new sequence from a single element.
+        /// </summary>
+        /// <param name="element">The only element of the sequence.</param>
+        /// <typeparam name="T">The <see cref="Type"/> of element in the the sequence.</typeparam>
+        /// <returns>An <see cref="IEnumerable{T}"/> containing only <paramref name="element"/>.</returns>
+        [Pure]
+        public static IEnumerable<T> Yield<T>(this T element)
+        {
+            yield return element;
+        }
+    }
+}
