a5f495069ab16591aac09f9b1aea33359be2c05e
Carnagion <carnagion.games@gmail.com>
2022-03-09 18:22:01 +0000

    Improve performance of ContainsAny<T> and ContainsAll<T>

--- Contains.cs
+++ Contains.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 
+using Bibliographer.Mla;
+
 namespace System.Linq
 {
     public static partial class IEnumerableExtensions
@@ -24,7 +26,7 @@ namespace System.Linq
             {
                 throw new ArgumentNullException(nameof(values));
             }
-            return source is HashSet<T> set ? values.Any(set.Contains) : values.Any(source.Contains);
+            return source is HashSet<T> set ? values.Any(set.Contains) : source.Intersect(values).Any();
         }
 
         /// <summary>
@@ -35,6 +37,11 @@ namespace System.Linq
         /// <typeparam name="T">The <see cref="System.Type"/> of elements in <paramref name="source"/> and <paramref name="values"/>.</typeparam>
         /// <returns><see langword="true"/> if all elements of <paramref name="values"/> are also contained in <paramref name="source"/>, else <see langword="false"/>.</returns>
         /// <exception cref="ArgumentNullException">Thrown if either <paramref name="source"/> or <paramref name="values"/> is <see langword="null"/>.</exception>
+        [Author("Eamon", "Nerbonne")]
+        [Title("Test whether two IEnumerable<T> have the same values with the same frequencies")]
+        [Container("StackOverflow")]
+        [Location("https://stackoverflow.com/questions/4576723/test-whether-two-ienumerablet-have-the-same-values-with-the-same-frequencies")]
+        [Accessed(2022, 3, 4)]
         [Pure]
         public static bool ContainsAll<T>(this IEnumerable<T> source, IEnumerable<T> values)
         {
@@ -46,7 +53,15 @@ namespace System.Linq
             {
                 throw new ArgumentNullException(nameof(values));
             }
-            return source is HashSet<T> set ? values.Any(set.Contains) : values.All(source.Contains);
+            
+            if (source is HashSet<T> set)
+            {
+                return values.All(set.Contains);
+            }
+
+            ILookup<T, T> sourceGroups = source.ToLookup(element => element);
+            ILookup<T, T> valuesGroups = values.ToLookup(element => element);
+            return (sourceGroups.Count == valuesGroups.Count) && sourceGroups.All(group => group.Count() == valuesGroups[group.Key].Count());
         }
     }
 }
