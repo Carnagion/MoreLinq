8eb44476a5ab534dc28233c8ba47278abaa52e9e
Carnagion <carnagion.games@gmail.com>
2022-03-12 14:47:56 +0000

    Add ForEach<T> overload

--- ForEach.cs
+++ ForEach.cs
@@ -27,5 +27,32 @@ namespace System.Linq
                 function.Invoke(item);
             }
         }
+
+        
+        /// <summary>
+        /// Executes <paramref name="function"/> over every item in <paramref name="source"/>.
+        /// </summary>
+        /// <param name="source">The <see cref="IEnumerable{T}"/> of items to execute <paramref name="function"/> on.</param>
+        /// <param name="function">The function to invoke for each item and its index in <paramref name="source"/>.</param>
+        /// <typeparam name="T">The <see cref="Type"/> of item in <paramref name="source"/>.</typeparam>
+        /// <exception cref="ArgumentNullException">Thrown if either <paramref name="source"/> or <paramref name="function"/> is <see langword="null"/>.</exception>
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T, int> function)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+            if (function is null)
+            {
+                throw new ArgumentNullException(nameof(function));
+            }
+
+            int index = 0;
+            foreach (T item in source)
+            {
+                function.Invoke(item, index);
+                index += 1;
+            }
+        }
     }
 }
