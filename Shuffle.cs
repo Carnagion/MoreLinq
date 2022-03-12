3f0e968a27b2c3d9803eaf8868ba65a751d10584
Carnagion <carnagion.games@gmail.com>
2022-03-12 15:46:08 +0000

    Add Shuffle<T> overload

--- Shuffle.cs
+++ Shuffle.cs
@@ -22,16 +22,25 @@ namespace System.Linq
         [Pure]
         public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source)
         {
-            if (source is null)
-            {
-                throw new ArgumentNullException(nameof(source));
-            }
-            
-            Random random = new();
-            return from element in from element in source
-                                   select (random.Next(), element)
-                   orderby element.Item1
-                   select element.Item2;
+            return source.Shuffle(new());
+        }
+
+        /// <summary>
+        /// Randomises the order of elements in <paramref name="source"/> using <paramref name="randomiser"/>.
+        /// </summary>
+        /// <param name="source">The <see cref="IEnumerable{T}"/> to shuffle.</param>
+        /// <param name="randomiser">The <see cref="Random"/> to use when shuffling elements.</param>
+        /// <typeparam name="T">The <see cref="Type"/> of element in <paramref name="source"/>.</typeparam>
+        /// <returns>An <see cref="IEnumerable{T}"/> containing all elements of <paramref name="source"/> in a random order.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="source"/> is <see langword="null"/>.</exception>
+        public static IEnumerable<T> Shuffle<T>(this IEnumerable<T> source, Random randomiser)
+        {
+            return source is null
+                ? throw new ArgumentNullException(nameof(source))
+                : from element in from element in source
+                                  select (randomiser.Next(), element)
+                  orderby element.Item1
+                  select element.Item2;
         }
     }
 }
