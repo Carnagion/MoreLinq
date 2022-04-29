ad28ea94bcc113a2a306ca302646391b75b5b43b
Carnagion <carnagion.games@gmail.com>
2022-04-29 18:37:04 +0100

    Add ToDictionary<TKey, TValue>()

---
+++ ToDictionary.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    public static partial class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Converts <paramref name="source"/> to a <see cref="Dictionary{TKey,TValue}"/>.
+        /// </summary>
+        /// <param name="source">The <see cref="IEnumerable{T}"/> of key-value pairs to convert.</param>
+        /// <typeparam name="TKey">The <see cref="Type"/> of keys.</typeparam>
+        /// <typeparam name="TValue">The <see cref="Type"/> of values.</typeparam>
+        /// <returns>A <see cref="Dictionary{TKey,TValue}"/> containing keys and values from <paramref name="source"/>.</returns>
+        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source) where TKey : notnull
+        {
+            return source.ToDictionary(entry => entry.Key, entry => entry.Value);
+        }
+    }
+}
